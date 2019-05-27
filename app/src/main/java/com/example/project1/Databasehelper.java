e3de02c77e7d0edccf58d42018af16c38120a6a0
Kundan <iemkundan@gmail.com>
2019-05-27 09:59:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/project1/Databasehelper.java
@@ -0,0 +1,70 @@
+package com.example.project1;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Databasehelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "parameters.db";
+    public static final String TABLE_NAME = "parameters_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "No_of_Range_Gate";
+    public static final String COL_3 = "No_of_Doppler_Filter";
+    public static final String COL_4 = "Frequency";
+    public static final String COL_5 = "No_of_Clear_PRF";
+    public static final String COL_6 = "Antenna_BeamWidth-Azimuth";
+    public static final String COL_7 = "Antenna_BeamWidth-Elevation";
+    public static final String COL_8 = "Minimum_Range";
+    public static final String COL_9 = "Maximum_Range";
+    public static final String COL_10 = "Target_Minimum_Velocity";
+    public static final String COL_11 = "Target_Maximum_Velocity";
+    public static final String COL_12 = "Pulse_Width";
+
+    public Databasehelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, No_of_Range_Gate INTEGER, No_of_Doppler_Filter INTEGER, Frequency INTEGER, No_of_Clear_PRF INTEGER, Antenna_BeamWidthAzimuth INTEGER, Antenna_BeamWidthElevation INTEGER, Minimum_Range INTEGER, Maximum_Range INTEGER, Target_Minimum_Velocity INTEGER, Target_Maximum_Velocity INTEGER, Pulse_Width INTEGER)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("Drop Table If Exists "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String No_of_Range_Gate, String No_of_Doppler_Filter, String Frequency, String No_of_Clear_PRF, String Antenna_BeamWidthAzimuth, String Antenna_BeamWidthElevation, String Minimum_Range, String Maximum_Range, String Target_Minimum_Velocity, String Target_Maximum_Velocity, String Pulse_Width)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,No_of_Range_Gate);
+        contentValues.put(COL_3,No_of_Doppler_Filter);
+        contentValues.put(COL_4,Frequency);
+        contentValues.put(COL_5,No_of_Clear_PRF);
+        contentValues.put(COL_6,Antenna_BeamWidthAzimuth);
+        contentValues.put(COL_7,Antenna_BeamWidthElevation);
+        contentValues.put(COL_8,Minimum_Range);
+        contentValues.put(COL_9,Maximum_Range);
+        contentValues.put(COL_10,Target_Minimum_Velocity);
+        contentValues.put(COL_11,Target_Maximum_Velocity);
+        contentValues.put(COL_12,Pulse_Width);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllData()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME, null);
+        return res;
+    }
+}
---
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.project1;
+
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+    Databasehelper mydb;
+    EditText ed1,ed2,ed3,ed4,ed5,ed6,ed7,ed8,ed9,ed10,ed11,ed12;
+    Button btnadd;
+    Button btnview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mydb = new Databasehelper(this);
+        ed1 = (EditText)findViewById(R.id.editText1);
+        ed2 = (EditText)findViewById(R.id.editText2);
+        ed3 = (EditText)findViewById(R.id.editText3);
+        ed4 = (EditText)findViewById(R.id.editText4);
+        ed5 = (EditText)findViewById(R.id.editText5);
+        ed6 = (EditText)findViewById(R.id.editText6);
+        ed7 = (EditText)findViewById(R.id.editText7);
+        ed8 = (EditText)findViewById(R.id.editText8);
+        ed9 = (EditText)findViewById(R.id.editText9);
+        ed10 = (EditText)findViewById(R.id.editText10);
+        ed11 = (EditText)findViewById(R.id.editText11);
+        ed12 = (EditText)findViewById(R.id.editText12);
+        btnadd = (Button)findViewById(R.id.button1);
+        btnview = (Button)findViewById(R.id.button2);
+        AddData();
+        ViewData();
+
+    }
+
+    public void AddData()
+    {
+        btnadd.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = mydb.insertData(ed1.getText().toString(),ed2.getText().toString(),ed3.getText().toString(),ed4.getText().toString(),ed10.getText().toString(),ed11.getText().toString(),ed6.getText().toString(),ed7.getText().toString(),ed8.getText().toString(),ed9.getText().toString(),ed12.getText().toString());
+                        if(isInserted = true)
+                            Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Data Not Inserted", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+    public void ViewData()
+    {
+        btnview.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = mydb.getAllData();
+                        if(res.getCount() == 0) {
+                            //show message
+                            showMessage("ERROR", "NO DATA FOUND");
+                            return;
+                        }
+
+                        StringBuffer buffer = new StringBuffer();
+                        while(res.moveToNext())
+                        {
+                            buffer.append("ID :"+ res.getString(0)+"\n");
+                            buffer.append("Range Gate :"+ res.getString(1)+"\n");
+                            buffer.append("Doppler Filter :"+ res.getString(2)+"\n");
+                            buffer.append("Frequency :"+ res.getString(3)+"\n");
+                            buffer.append("Clear PRF :"+ res.getString(4)+"\n");
+                            buffer.append("Azimuth Antenna BeamWidth :"+ res.getString(5)+"\n");
+                            buffer.append("Elevation Antenna BeamWidth :"+ res.getString(6)+"\n");
+                            buffer.append("Minimum Range :"+ res.getString(7)+"\n");
+                            buffer.append("Maximum Range :"+ res.getString(8)+"\n");
+                            buffer.append("Target Minimum Velocity :"+ res.getString(9)+"\n");
+                            buffer.append("Target Maximum Velocity :"+ res.getString(10)+"\n");
+                            buffer.append("Pulse Width :"+ res.getString(11)+"\n\n");
+
+
+                        }
+                        showMessage("DATA", buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String Title, String Message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(Title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+}
